9dd849a8131c0b5f97e111a2ca0c1f5b9cfbcd52
artiely <1119696785@qq.com>
2018-03-17 15:29:26 +0800

    添加自动发布脚本

--- server/server.js
+++ server/server.js
@@ -29,7 +29,9 @@ io.on('connection', function (socket) {
       to,
       content
     } = data
-    const chatid = [from, to].sort().join('-')
+    const chatid = [from, to]
+      .sort()
+      .join('-')
     Chat.create({
       from,
       to,
@@ -58,12 +60,11 @@ io.on('connection', function (socket) {
     }, {
       'multi': true
     }, function (err, doc) {
-      // 入库之后全局广播
-      // io.emit('recvmsg', ...d)
+      // 入库之后全局广播 io.emit('recvmsg', ...d)
     })
-    
+
   })
-    // 下单
+  // 下单
   socket.on('sendorder', function (data) {
     const {
       userinfo,
@@ -85,72 +86,113 @@ io.on('connection', function (socket) {
     })
   })
   // 接单
-  socket.on('acceptorder',function(data){
-    Order.findByIdAndUpdate(data.oid,{$set:{status:1,engInfo:data.engInfo},$push:{times:new Date().getTime()}}, {new:true},function(err,doc){
-      if(!err){
-        console.log('接单了',doc)
-        io.emit('recvorder', doc)
-        /////
-        const from = doc.engInfo._id
-        const to = doc.userinfo._id
-        const chatid = [from, to].sort().join('-')
-        const content = `您的订单已被${doc.engInfo.username}接受,请您尽快联系我`
-        Chat.create({
-          from,
-          to,
-          content,
-          chatid,
-          create_time: new Date().getTime()
-        }, function (err, doc) {
-          // 入库之后全局广播
-          io.emit('recvmsg', doc)
-          console.log('入库的消息', doc)
-        })
-        /////
-      }
-      console.log(err)
-    })
+  socket.on('acceptorder', function (data) {
+    Order
+      .findByIdAndUpdate(data.oid, {
+        $set: {
+          status: 1,
+          engInfo: data.engInfo
+        },
+        $push: {
+          times: new Date().getTime()
+        }
+      }, {
+        new: true
+      }, function (err, doc) {
+        if (!err) {
+          console.log('接单了', doc)
+          io.emit('recvorder', doc)
+          /////
+          const from = doc.engInfo._id
+          const to = doc.userinfo._id
+          const chatid = [from, to]
+            .sort()
+            .join('-')
+          const content = `您的订单已被${doc.engInfo.username}接受,请您尽快联系我`
+          Chat.create({
+            from,
+            to,
+            content,
+            chatid,
+            create_time: new Date().getTime()
+          }, function (err, doc) {
+            // 入库之后全局广播
+            io.emit('recvmsg', doc)
+            console.log('入库的消息', doc)
+          })
+          /////
+        }
+        console.log(err)
+      })
   })
   // 取消
-  socket.on('delorder', function(data) {
-    console.log('123',data)
-    Order.findByIdAndUpdate(data._id,{$set:{status:-1}},{new:true}).exec(function(err,doc){
-      if(!err){
-        console.log('取消了',doc)
-        io.emit('recvorder', doc)
-      }
-      console.log(err)
-    })
+  socket.on('delorder', function (data) {
+    console.log('123', data)
+    Order.findByIdAndUpdate(data._id, {
+        $set: {
+          status: -1
+        }
+      }, {
+        new: true
+      })
+      .exec(function (err, doc) {
+        if (!err) {
+          console.log('取消了', doc)
+          io.emit('recvorder', doc)
+        }
+        console.log(err)
+      })
   })
   // 工程师确认完成
-  socket.on('confirmbyengineer', function(data) {
-    Order.findByIdAndUpdate(data._id,{$set:{status:2}},{new:true}).exec(function(err,doc){
-      if(!err){
-        console.log('工程师确认完成了',doc)
-        io.emit('recvorder', doc)
-      }
-      console.log(err)
-    })
+  socket.on('confirmbyengineer', function (data) {
+    Order.findByIdAndUpdate(data._id, {
+        $set: {
+          status: 2
+        }
+      }, {
+        new: true
+      })
+      .exec(function (err, doc) {
+        if (!err) {
+          console.log('工程师确认完成了', doc)
+          io.emit('recvorder', doc)
+        }
+        console.log(err)
+      })
   })
   // 客户确认完成
-  socket.on('confirmbycustom', function(data) {
-    Order.findByIdAndUpdate(data._id,{$set:{status:3}},{new:true}).exec(function(err,doc){
-      if(!err){
-        console.log('客户确认完成了',doc)
-        io.emit('recvorder', doc)
-      }
-      console.log(err)
-    })
+  socket.on('confirmbycustom', function (data) {
+    Order.findByIdAndUpdate(data._id, {
+        $set: {
+          status: 3
+        }
+      }, {
+        new: true
+      })
+      .exec(function (err, doc) {
+        if (!err) {
+          console.log('客户确认完成了', doc)
+          io.emit('recvorder', doc)
+        }
+        console.log(err)
+      })
   })
   // 客户评论
-  socket.on('evaluate', function(data) {
-    Order.findByIdAndUpdate(data._id,{$set:{status:4}},{new:true}).exec(function(err,doc){
-      if(!err){
-        console.log('客户评论了',doc)
-        io.emit('recvorder', doc)
-      }
-      console.log(err)
-    })
+  socket.on('evaluate', function (data) {
+    Order.findByIdAndUpdate(data._id, {
+        $set: {
+          status: 4
+        }
+      }, {
+        new: true
+      })
+      .exec(function (err, doc) {
+        if (!err) {
+          console.log('客户评论了', doc)
+          io.emit('recvorder', doc)
+        }
+        console.log(err)
+      })
   })
 })
 app.use(cookieParser())
